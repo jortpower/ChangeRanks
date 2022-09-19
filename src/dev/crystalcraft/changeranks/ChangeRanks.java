44562095addee09889deda7e5edc3f203fb9dbd9
jortpower <jortpower2009@gmail.com>
2022-09-19 19:10:24 +0200

    the bare bones

---
+++ src/dev/crystalcraft/changeranks/ChangeRanks.java
@@ -0,0 +1,10 @@
+package dev.crystalcraft.changeranks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ChangeRanks extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+}
